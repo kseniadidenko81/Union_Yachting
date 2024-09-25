bb2ea56d6a60c11d1016ff2b9bbb01cd164b8a71
kseniadidenko81 <ksenia.vd81@gmail.com>
2024-09-25 06:50:17 +0300

    first

---
+++ js/index-js.js
@@ -0,0 +1,17 @@
+(function ($) {
+
+  var thumbnails = document.getElementById("thumbnails")
+  var imgs = thumbnails.getElementsByTagName("img")
+  var main = document.getElementById("main-img")
+  var counter=0;
+
+  for(let i=0;i<imgs.length;i++){
+    let img=imgs[i]
+
+
+    img.addEventListener("click",function(){
+      main.src=this.src
+    })
+
+  }
+})(jQuery);

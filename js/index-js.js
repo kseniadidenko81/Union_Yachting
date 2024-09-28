18ae897339f598fe2048ba2420208271d90fd809
kseniadidenko81 <ksenia.vd81@gmail.com>
2024-09-28 23:02:29 +0300

    firstsm

--- js/index-js.js
+++ js/index-js.js
@@ -1,18 +1,12 @@
-var thumbnails = document.getElementById("thumbnails")
-var imgs = thumbnails.getElementsByTagName("img")
-var main = document.getElementById("main-img")
-var counter=0;
+var thumbnails = document.getElementById("thumbnails");
+var imgs = thumbnails.getElementsByTagName("img");
+var main = document.getElementById("main-img");
+var counter = 0;
 
-for(let i=0;i<imgs.length;i++){
-  let img=imgs[i]
+for (let i = 0; i < imgs.length; i++) {
+  let img = imgs[i];
 
-
-  img.addEventListener("click",function(){
-    main.src=this.src
-  })
-
-<<<<<<< HEAD
+  img.addEventListener("click", function () {
+    main.src = this.src;
+  });
 }
-=======
-}
->>>>>>> 02e7d8354cf46d1d32ac3ae0ab6c20e1d6569677
